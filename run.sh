2807aa379c3aef30221a40fd6c65f0e160a83875
Pedro N <itiropedro@gmail.com>
2026-05-22 19:00:58 -0300

    Update run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+LLMORPHEUS_LLM_API_ENDPOINT="http://localhost:8080/v1/chat/completions" \
+LLMORPHEUS_LLM_AUTH_HEADERS='{"Content-Type": "application/json"}' \
+npx -p node@20 node benchmark/createMutants.js \
+--path 'samples' \
+--mutate index.ts \
+--model gemma-4-e2b-it \
+--maxTokens 2000 \
+--template 'templates/template-basic.hb'
