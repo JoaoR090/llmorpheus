fda643432cafed3f4cd1b3d5edf63852e39f2d5b
Pedro N <itiropedro@gmail.com>
2026-06-10 16:18:40 -0300

    a

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 npm run build
-LLMORPHEUS_LLM_API_ENDPOINT="http://localhost:8080/v1/chat/completions" \
+LLMORPHEUS_LLM_API_ENDPOINT="http://127.0.0.1:1234/v1/chat/completions" \
 LLMORPHEUS_LLM_AUTH_HEADERS='{"Content-Type": "application/json"}' \
 npx -p node@20 node benchmark/createMutants.js \
 --path 'samples' \
 --mutate index.ts \
---model gemma-4-e2b-it \
+--model gemma-4-12b-qat \
 --maxTokens 2000 \
 --template 'templates/template-basic.hb'
