44e24a20093ce62e6b1cb01fd5f29addfb9bb6c6
JoaoR090 <jrzr432@gmail.com>
2026-06-10 14:30:31 -0300

    commit-20260610-143031-3722

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+npm run build
 LLMORPHEUS_LLM_API_ENDPOINT="http://localhost:8080/v1/chat/completions" \
 LLMORPHEUS_LLM_AUTH_HEADERS='{"Content-Type": "application/json"}' \
 npx -p node@20 node benchmark/createMutants.js \
