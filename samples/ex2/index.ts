a2aacd628d49f4703f1c78d6bfd566065f2379fa
JoaoR090 <jrzr432@gmail.com>
2026-06-17 14:50:59 -0300

    commit-20260617-145059-17371

--- samples/ex2/index.ts
+++ samples/ex2/index.ts
@@ -1,5 +1,10 @@
-const idade: number = 20;
 const nome = "João";
+const idade = 20;
+
+const pessoa = {
+  nome: "João",
+  idade: 20,
+};
 
 function podeEntrar(idade: number): boolean {
   if (idade >= 18) {
@@ -17,17 +22,6 @@ while (idade > 0) {
   break;
 }
 
-const frutas = ["maçã", "banana", "laranja"];
-
-for (const fruta of frutas) {
-  console.log(fruta);
-}
-
-const pessoa = {
-  nome: "João",
-  idade: 20,
-};
-
 for (const chave in pessoa) {
   console.log(chave);
 }
@@ -40,7 +34,3 @@ switch (nome) {
   default:
     console.log("Não encontrado");
 }
-
-const dobrar = (x: number) => x * 2;
-
-const resultado = dobrar(10);
