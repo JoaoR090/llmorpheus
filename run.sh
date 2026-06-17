e73863ff756dea2bbaae527197375c2821a2da33
JoaoR090 <jrzr432@gmail.com>
2026-06-17 16:06:06 -0300

    commit-20260617-160606-4001

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ npm run build
 LLMORPHEUS_LLM_API_ENDPOINT="http://127.0.0.1:1234/v1/chat/completions" \
 LLMORPHEUS_LLM_AUTH_HEADERS='{"Content-Type": "application/json"}' \
 npx -p node@20 node benchmark/createMutants.js \
---path 'samples/ex2' \
+--path 'samples/ex1' \
 --mutate index.ts \
 --model gemma-4-12b-qat \
 --maxTokens 5000 \
