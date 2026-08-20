213ee8efcbcefe8dd90fb3e1e3f0ab8d9f875c00
JoaoR090 <jrzr432@gmail.com>
2026-08-20 10:31:19 -0300

    commit-20260820-103119-20533

---
+++ scripts/clone-pr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# uso: ./clone_pr.sh <url> [pasta-destino]
+
+URL="$1"
+REPO="${URL##*/}"
+REPO="${REPO%.git}"
+
+BASE_DEST="${2:-.}"
+DEST="$BASE_DEST/$REPO"
+
+(mkdir -p "$BASE_DEST" &> /dev/null) || exit 1
+
+(git clone --single-branch "$URL" "$DEST" &> /dev/null) || exit 1
+cd "$DEST" || exit 1
+git checkout HEAD~1  &> /dev/null
