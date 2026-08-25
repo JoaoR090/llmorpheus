e915e2987564d42c4da6b3bc40f5c08f4e188c7e
JoaoR090 <jrzr432@gmail.com>
2026-08-25 11:32:09 -0300

    Commit em 25/08/2026 às 11:32:09

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@ npm run build
 LLMORPHEUS_LLM_API_ENDPOINT="http://127.0.0.1:8080/v1/chat/completions" \
 LLMORPHEUS_LLM_AUTH_HEADERS='{"Content-Type": "application/json"}' \
 npx -p node@20 node benchmark/createMutants.js \
---path 'clone_prs/tests-node-backend-tests-using-jest-typescript/src' \
+--path 'scripts/clone_prs/tests-node-backend-tests-using-jest-typescript/src' \
 --mutate * \
 --model gemma-4-12b-qat \
 --maxTokens 5000 \
